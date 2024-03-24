37c37c3324528e04f73c5bde2f956f42dcc74e55
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2024-03-24 04:32:00 +0200

    fix and change to server

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const app = express()
 app.use(express.static(path.join(__dirname,'assets')))
 app.set('view engine','ejs')
 app.set('views','views')
+
 app.get('/',(req,res,next)=>{
-    res.send('hello')
+    res.render('index')
 })
 
 app.listen(3000,()=>{
