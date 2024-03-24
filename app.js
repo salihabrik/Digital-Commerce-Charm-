d0cf58147bb5c8203f5e98997cb7dd34ae78781c
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2024-03-24 03:11:03 +0200

    create app .js backend store

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path =require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname,'assets')))
+app.set('view engine','ejs')
+app.set('views','views')
+app.get('/',(req,res,next)=>{
+    res.send('hello')
+})
+
+app.listen(3000,()=>{
+    
+console.log('server listen on port 3000')
+})
